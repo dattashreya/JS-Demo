8ed26b4a90a8038bf8308df05db81855c402fb6c
dattashreya <159568371+dattashreya@users.noreply.github.com>
2026-03-27 15:44:50 +0530

    Create a foreach loop and print the object values of array
    Create a foreach loop and print the object values of array

---
+++ foreach_arr.js
@@ -0,0 +1,10 @@
+const arr = [
+    {id: 1, name: 'pizza', price: 50},
+    {id: 2, name: 'burger', price: 60},
+    {id: 3, name: 'sandwitch', price: 150}
+];
+
+let i=0;
+arr.forEach(i=> {
+    console.log(i.price);    
+})
